9405aa99b9ae794bde1db7822733438a43525e86
Chevuu <vukjurisic@gmail.com>
2023-11-04 16:53:40 +0000

    made some practice Slot machine logic

---
+++ client/src/machine-logic/GridGeneration.js
@@ -0,0 +1,64 @@
+export function generateOneFullLineGrid(rows, columns, symbols, num) {
+    const initialGrid = [];
+  
+    for (let i = 0; i < rows; i++) {
+      const row = [];
+      let crookedSymbol;
+      let randomSymbol = symbols[0];
+      
+      // Define probabilities for the custom line specified by `num`
+      const randomProbability = 0.20;
+      const firstTwoSameProbability = 0.20;
+      const firstThreeSameProbability = 0.20;
+      const firstFourSameProbability = 0.20;
+  
+      for (let j = 0; j < columns; j++) {
+        // Generate a random number between 0 and 1
+        const random = Math.random();
+        
+        if (i === num - 1) {
+          // Check the probability conditions for the custom line
+          if (random < randomProbability) {
+            // 25% chance for fully random symbol
+            crookedSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+          } else if (random < randomProbability + firstTwoSameProbability) {
+            // 25% chance for the first two symbols to be the same
+            if (j < 2) {
+              crookedSymbol = randomSymbol;
+            } else {
+              crookedSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+              randomSymbol = crookedSymbol;
+            }
+          } else if (random < randomProbability + firstTwoSameProbability + firstThreeSameProbability) {
+            // 20% chance for the first three symbols to be the same
+            if (j < 3) {
+              crookedSymbol = randomSymbol;
+            } else {
+              crookedSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+              randomSymbol = crookedSymbol;
+            }
+          } else if (random < randomProbability + firstTwoSameProbability + firstThreeSameProbability + firstFourSameProbability) {
+            // 15% chance for the first four symbols to be the same
+            if (j < 4) {
+              crookedSymbol = randomSymbol;
+            } else {
+              crookedSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+              randomSymbol = crookedSymbol;
+            }
+          } else {
+            // 10% chance for all five symbols to be the same (same as before)
+            crookedSymbol = randomSymbol;
+          }
+        } else {
+          // For other rows, stay random
+          crookedSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+        }
+  
+        row.push(crookedSymbol);
+      }
+      initialGrid.push(row);
+    }
+  
+    return initialGrid;
+  }
+  
---
+++ client/src/machine-logic/SlotMachineLogic.js
@@ -0,0 +1,56 @@
+export function checkForWinningCombination(grid, betValue, winningLineCount) {
+  let winningLines = [];
+  let multiplier = 0;
+  if (winningLineCount === 5) {
+    winningLines = [
+      [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]],
+      [[0, 1], [1, 1], [2, 1], [3, 1], [4, 1]],
+      [[0, 2], [1, 2], [2, 2], [3, 2], [4, 2]],
+      [[0, 0], [1, 1], [2, 2], [3, 1], [4, 0]],
+      [[0, 2], [1, 1], [2, 0], [3, 1], [4, 2]],
+    ];
+  }
+
+  // logGrid(grid);
+
+  const winningLineInfo = [];
+
+  for (const line of winningLines) {
+    const [x, y] = line[0];
+    const firstItem = grid[y][x];
+    let connected = 0;
+
+    for (const [dx, dy] of line) {
+      const symbol = grid[dy][dx];
+      if (symbol !== firstItem) {
+        break;
+      }
+      connected++;
+    }
+    if (connected === 3) multiplier += 1;
+    if (connected === 4) multiplier += 3;
+    if (connected === 5) multiplier += 10;
+
+    if (connected >= 3) {
+      winningLineInfo.push({ line, connected });
+    }
+  }
+
+  console.log("Final multiplier: " + multiplier);
+  return { totalMultiplier: betValue * multiplier, winningLines: winningLineInfo };
+}
+
+// function logGrid(grid) {
+//   console.log("Grid shape:");
+//   for (let i = 0; i < grid.length; i++) {
+//     let rowString = "| ";
+//     for (let j = 0; j < grid[i].length; j++) {
+//       rowString += grid[i][j] + "," + i + "," + j;
+//       if (j < grid[i].length - 1) {
+//         rowString += " | ";
+//       }
+//     }
+//     rowString += " |";
+//     console.log(rowString);
+//   }
+// }
